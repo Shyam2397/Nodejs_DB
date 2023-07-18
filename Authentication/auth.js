4061d44aeabfb14625ec268fba05e44ba64656c6
Shyam sundar R <shyamsundarr10@gmail.com>
2023-07-18 21:24:27 +0530

    dot changes

--- Authentication/auth.js
+++ Authentication/auth.js
@@ -3,9 +3,9 @@ import jwt from "jsonwebtoken";
 export function authMiddleware(req,res,next){
     const token = req.headers["x-auth-token"]
     if(!token){
-        res.status(400),send({message:"Invalid autherization"})
+        res.status(400).send({message:"Invalid autherization"})
     }
     jwt.verify(token,process.env.SECRET_KEY)
-    
+
     next();
 }
