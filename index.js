1d0b4a51525b8e1e38ed4d210f73a9fa2ab653b2
Shyam sundar R <shyamsundarr10@gmail.com>
2023-07-18 20:13:06 +0530

    user router is create

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import { studentsRouter } from "./Routes/student.js";
 import dotenv from "dotenv";
+import { userRouter } from "./Routes/user.js";
 // initializing a express server
 
 const app = express();
@@ -19,6 +20,7 @@ app.use(cors());
 
 //application routes
 app.use("/students", studentsRouter);
+app.use("/user",userRouter);
 
 //start the server
 app.listen(PORT, () => console.log(`Server started in localhost : ${PORT}`));
